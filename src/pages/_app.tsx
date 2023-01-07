04a904a6eff74739dca7fb7493cdbdda1e57b2d4
antonpalermo <ANPBusinessmail@gmail.com>
2023-01-07 22:24:13 +0800

    initialize tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
+import '@styles/globals'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
